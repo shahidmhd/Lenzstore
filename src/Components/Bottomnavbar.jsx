a1300534eaa53b59a22bf99875595327a1ba99b1
shahidmhd <shahidvk124@gmail.com>
2023-12-08 15:05:47 +0530

    latest oneeeti

--- src/Components/Bottomnavbar.jsx
+++ src/Components/Bottomnavbar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { CgMenuGridR } from "react-icons/cg";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
 import './BottomNavbar.css';
 
 const Bottomnavbar = () => {
@@ -11,25 +16,28 @@ const Bottomnavbar = () => {
           <Nav.Item>
             <Link to="/" className="nav-link">
               {/* Icon 1 */}
-              <i className="ion-android-person" style={{ color: "#fff" }} />
+              <CgMenuGridR  style={{ color: 'white' }} />
             </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link to="/page2" className="nav-link">
+            <Link to="/cart" className="nav-link">
               {/* Icon 2 */}
-              <i className="ion-android-person" style={{ color: "#fff" }} />
+              {/* <i className="ion-android-person" style={{ color: "#fff" }} /> */}
+              <MdOutlineShoppingBag  style={{ color: 'white' }}/>
             </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link to="/page3" className="nav-link">
+            <Link to="/login" className="nav-link">
               {/* Icon 3 */}
-              <i className="ion-android-person" style={{ color: "#fff" }} />
+              {/* <i className="ion-android-person" style={{ color: "#fff" }} /> */}
+              <CgProfile style={{ color: 'white' }}/>
             </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link to="/page4" className="nav-link">
+            <Link to="/" className="nav-link">
               {/* Icon 4 */}
-              <i className="ion-android-person" style={{ color: "#fff" }} />
+              {/* <i className="ion-android-person" style={{ color: "#fff" }} /> */}
+              <BiCategory style={{ color: 'white' }} />
             </Link>
           </Nav.Item>
         </Nav>
